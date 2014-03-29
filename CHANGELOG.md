32b0a9d5421b306499ffe1ce511534f66562c097
Dimitar Dimitrov <me@ddimitrov.name>
2014-03-29 23:30:48 +0200

    Default bedrock installation

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+### 1.1.1: 2014-03-11
+
+* Update phpdotenv to 1.0.6
+* Update wp-cli to v0.14.1
+* Update README to refence new WordPress Packagist namespaces
+* Fix uploads path in `linked_dirs` for Capistrano deploys
+
+### 1.1.0: 2014-03-01
+
+* Update to Capistrano 3.1.0: `deploy:restart` is no longer run by default
+* Better webroot structure: introduces the `/web` directory as the document/web root for web server vhosts
+
+### 1.0.0: 2013-12-18
+
+* Initial release
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
+
+# Branch options
+# Prompts for the branch name (defaults to current branch)
+#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Sets branch to current one
+#set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Hardcodes branch to always be master
+# This could be overridden in a stage config file
+set :branch, :master
+
+set :deploy_to, "/srv/www/#{fetch(:application)}"
+
+set :log_level, :info
+
+set :linked_files, %w{.env}
+set :linked_dirs, %w{web/app/uploads}
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :service, :nginx, :reload
+    end
+  end
+end
+
+# The above restart task is not run by default
+# Uncomment the following line to run it on deploys if needed
+# after 'deploy:publishing', 'deploy:restart'
