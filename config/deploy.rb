1b6f0fa1da6583d5315b4c77443c0efd0635049b
Dimitar Dimitrov <me@ddimitrov.name>
2014-03-30 17:05:23 +0300

    Preserve WP's .htaccess across deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,8 @@ set :deploy_to, "/home/openmedia/#{fetch(:application)}"
 
 set :log_level, :info
 
-set :linked_files, %w{.env}
-set :linked_dirs, %w{web/app/uploads}
+set :linked_files, %w(.env web/.htaccess)
+set :linked_dirs, %w(web/app/uploads)
 
 namespace :deploy do
   desc 'Restart application'
