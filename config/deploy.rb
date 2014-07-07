01ecaa69a1667ba399e4952c751c670e39dc1ede
Dimitar Dimitrov <me@ddimitrov.name>
2014-07-07 11:32:28 +0300

    Symlink the custom CSS folder of revslider under shared/
    The revslider plugin keeps some permanent files in this folder so we
    need to preserve this directory during deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,24 @@ set :linked_files, %w(.env web/.htaccess)
 set :linked_dirs, %w(web/app/uploads backups)
 
 namespace :deploy do
+  desc 'Keep folders which plugins persist their files in symlinked under shared/'
+  task :symlink_plugin_persistant_folders do
+    folders = %w(web/app/plugins/revslider/rs-plugin/css)
+
+    on roles(:app) do
+      folders.each do |folder_to_persist|
+        unless test "[[ -d #{shared_path.join(folder_to_persist)} ]] "
+          execute :cp, '-r', release_path.join(folder_to_persist), shared_path.join(folder_to_persist)
+        end
+
+        execute :rm, '-rf', release_path.join(folder_to_persist)
+        execute :ln, '-s', shared_path.join(folder_to_persist), release_path.join(folder_to_persist)
+      end
+    end
+  end
+
+  before 'deploy:updated', 'deploy:symlink_plugin_persistant_folders'
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
